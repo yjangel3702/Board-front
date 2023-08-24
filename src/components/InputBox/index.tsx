dd1c17d68a73f530d75d9e9c176ba3c494052612
kimyujung <yjangel3702@gmail.com>
2023-08-24 09:16:43 +0900

    commit

--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -1,20 +1,38 @@
 import React from 'react';
 import './style.css';
 
+//          interface: Input 상자 컴포넌트 Props          //
+interface Props {
+  label: string;
+  type: 'text' | 'password';
+  placeholder: string;
+  icon?: string;
+  errorMessage?: string;
+  onButtonClick?: () => void;
+
+}
+
 //          component: Input 상자 컴포넌트          //
-export default function InputBox() {
+export default function InputBox(props: Props) {
+
+  //          state: Properties         //
+  const { label, type, placeholder, icon, errorMessage } = props;
+  const { onButtonClick } = props;
 
   //          render: Input 상자 렌더링         //
   return (
     <div className="inputbox">
-      <div className='inputbox-label'>주소*</div>
-      <div className='inputbox-container'>
-        <input className='input' placeholder='우편번호 찾기'/>
-        <div className='icon-button'>
-          <div className='right-arrow-icon'></div>
-        </div>
+      <div className='inputbox-label'>{label}</div>
+      <div className={errorMessage ? 'inputbox-container-error' : 'inputbox-container'}>
+        <input className='input' type={type} placeholder={placeholder}/>
+        { onButtonClick !== undefined && (
+          <div className='icon-button' onClick={onButtonClick}>
+            {icon}
+            <div className='right-arrow-icon'></div>
+          </div>
+        ) }
       </div>
-      <div className='inputbox-message'>우편번호를 선택해주세요.</div>
+      <div className='inputbox-message'>{errorMessage}</div>
     </div>
   )
 }
