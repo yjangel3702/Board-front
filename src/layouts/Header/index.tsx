3b46ed06b024f08d5ac2bca81995decbe50496a2
kimyujung <yjangel3702@gmail.com>
2023-08-28 13:45:55 +0900

    나 집가고 싶어

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import './style.css';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 //          component: 헤더 컴포넌트          //
 export default function Header() {
+
+  //          state: path name 상태         //
+  const { pathname } = useLocation();
+
+  //          variable: 인증 페이지 논리 변수         //
+  const isAuthPage = pathname === '/auth';
+  //          variable: 메인 페이지 논리 변수         //
+  const isMainPage = pathname === '/';
+  //          variable: 검색 페이지 논리 변수         //
+  const isSearchPage = pathname === '/search';
+  //          variable: 게시물 상세 페이지 논리 변수         //
+  const isBoardDetailPage = pathname === '/board/detail';
+  //          variable: 유저 페이지 논리 변수         //
+  const isUserPage = pathname === '/user';
+  //          variable: 게시물 작성 페이지 논리 변수         //
+  const isBoardWritePage = pathname === '/board/write';
+  //          variable: 게시물 작성 페이지 논리 변수         //
+  const isBoardUpdatePage = pathname === '/board/update';
 
   //          function: 네비게이트 함수          //
   const navigator = useNavigate();
@@ -11,6 +29,43 @@ export default function Header() {
   //          event handler: 로고 클릭 이벤트 처리          //
   const onLogoClickHandler = () => {
     navigator('/')
+  }
+
+  //          component: 검색 컴포넌트          //
+  const Search = () => {
+    //          state: 검색 버튼 상태          //
+    const [showInput, setShowInput] = useState<boolean>(false);
+    //          state: 검색 값 상태          //
+    const [searchValue, setSearchValue] = useState<string>('');
+
+    //          event handler: 검색 값 변경 이벤트 처리          //
+    const onSearchValueChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+      const searchValue = event.target.value;
+      setSearchValue(searchValue);
+    }
+
+    //          event handler: 검색 버튼 클릭 이벤트 처리          //
+    const onSearchButtonClickHandler = () => {
+      setShowInput(!showInput);
+    }
+
+    //          render: 검색 컴포넌트 렌더링          //
+    if (showInput) 
+    return (
+      <div className='header-search-input-box'>
+        <input className='header-search-input' type='text' value={searchValue} onChange={onSearchValueChangeHandler} />
+        <div className='icon-button' onClick={onSearchButtonClickHandler}>
+          <div className='search-icon'></div>
+        </div>
+      </div>
+    );
+
+    return (
+      <div className='icon-button' onClick={onSearchButtonClickHandler}>
+        <div className='search-icon'></div>
+      </div> 
+    );
+
   }
 
   //          render: 헤더 컴포넌트 렌더링          //
@@ -25,7 +80,13 @@ export default function Header() {
         </div>
 
         <div className='header-right-box'>
-          
+          { isAuthPage && (<Search />) }
+          { isMainPage && (<></>) }
+          { isSearchPage && (<></>) }
+          { isBoardDetailPage && (<></>) }
+          { isUserPage && (<></>) }
+          { isBoardWritePage && (<></>) }
+          { isBoardUpdatePage && (<></>) }
         </div>
       </div>
     </div>
